d9b9d90f9bd52343e13ff25cf42b0606f64852cb
Mahmut P <109034196+C0derByM4H6301@users.noreply.github.com>
2022-12-18 11:50:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import  sys
+import sys
+import time
+import os
 print("Mah-Editor")
-print("Exit for '--END'")
+print("Exit for '<END>'")
 source_code = ""
 while True:
     line = sys.stdin.readline()
-    if line.startswith("--END"):
+    if line.startswith("<END>"):
         break
     source_code += line
 
 f = open("text.txt","w+")
 f.write(source_code)
+def t():
+    current_time = time.localtime()
+    ctime = time.strftime('%H:%M:%S', current_time)
+    return '[' + ctime + ']'
+user = os.getlogin( )
+f.write(f"\n{t()} , Edited for user: {user}\n")
 f.close()
